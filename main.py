e98923ddc32820ef3987e8b987bc3de356491115
noviccio <mnovicio2001@gmail.com>
2023-11-07 00:03:28 -0500

    added main function + gitignore

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from google.oauth2 import service_account 
 from googleapiclient.discovery import build 
 import pytz 
-
-def main():
-    print('the supported timezones by the pytz module:',
-      pytz.all_timezones, '\n')
+import datetime
+from twilio.rest import Client
 
 def get_google_calendar():
     scopes = ["https://www.googleapis.com/auth/cloud-platform.read-only"]
@@ -19,23 +17,60 @@ def get_google_calendar():
         'calendar',
         'v3',
         credentials = creds
-    )
+    ) 
 
     return service
 
-print('the supported timezones by the pytz module:',
-      pytz.all_timezones, '\n')
 
-def get_events():
+def get_events(calendar_service):
     est = pytz.timezone('America/New York')
     now = datetime.now(est)
+    end_of_day = datetime(now.year, now.month, now.day, 23, 0, 0)
+
+    events_result = calendar_service.events().list(
+        calendarId = 'primary',
+        timeMin=now.isoformat(),
+        timeMax=end_of_day.isoformat(),
+        singleEvents=True,
+        orderBy='startTime'
+    ).execute()
+
+    events = events_result.get(
+        'items',
+        []
+    )
+
+    return events
+
+def text_schedule(schedule):
+    TWILIO_SID = 'TWILIO_SID'
+    TWILIO_AUTH_TOKEN = 'AUTH_TOKEN'
+    TWILIO_PHONE_NUMBER = 'PHONE'
+    RECIPIENT_PHONE_NUMBER = 'PHONE'
+
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    if not schedule:
+        message = "You have no events scheduled for today."
+    else:
+        message = "Your schedule for today:\n"
+        for event in schedule: 
+            start_time = event['start'].get('dateTime', event['start'].get('date'))
+            end_time = event['end'].get('dateTime', event['end'].get('date'))
+            event_summary = event['summary']
+            message = f"{start_time} - {end_time}: {event_summary}\n"
+
+            message = client.messages.create(
+                body=message,
+                from_=TWILIO_PHONE_NUMBER,
+                to=RECIPIENT_PHONE_NUMBER
+            )
 
 
-
-
-
-
-
+def main():
+   cal_service = get_google_calendar()
+   schedule = get_events(cal_service)
+   text_schedule(schedule)
 
 
 
